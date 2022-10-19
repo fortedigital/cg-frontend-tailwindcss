f13f4346abc45863c0b64c9b0bd4ce1e3e1a4b23
AstIndr <astrid.indrelid@gmail.com>
2022-10-19 14:22:42 +0200

    Add start setup

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <div>
+          <a href="/">LOGO</a>
+          <div>
+            <a href="/om-oss">Om oss</a>
+            <a href="/prosjekter">Tjenester</a>
+            <a href="/prosjekter">Prosjekter</a>
+            <a href="/jobb">Jobb</a>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <h1>Drevet av gammeldags innovasjon</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
