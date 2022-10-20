509643c88d51220238eaa571a3fe340eade337d7
Eric Veliyulin <eric.vel@outlook.com>
2022-10-20 10:27:35 +0200

    Update readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "retro-white": "#FFF8EA",
-        "retro-coral": "#9E7676",
-        "retro-brown": {
-          DEFAULT: "#594545",
-          light: "#815B5B",
-          dark: "#3e3030",
-        },
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      "retro-white": "#FFF8EA",
+      "retro-coral": "#9E7676",
+      "retro-brown": {
+        DEFAULT: "#594545",
+        light: "#815B5B",
+        dark: "#3e3030",
       },
     },
+    extend: {},
   },
   plugins: [],
 };
